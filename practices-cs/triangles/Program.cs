ac89b3fbb1f7d9fa25b338ab305cc422da25932e
Jaco Minnaar <jaco@jacominnaar.com>
2024-09-11 08:43:46 +0200

    this is a thing

---
+++ practices-cs/triangles/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+var input = Console.ReadLine();
+var rows = int.Parse(input);
+int i = 0;
+while (i < rows)
+{
+    int j = 0;
+    while (j < rows - i)
+    {
+        Console.Write("*");
+        j = j + 1;
+    }
+    Console.WriteLine();
+
+    i = i + 1;
+}
