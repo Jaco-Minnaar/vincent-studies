38fea30d76af1bd7c2a8681efc2c787d8bb4eeb1
Jaco Minnaar <jminnaar@bscglobal.com>
2024-09-11 09:04:54 +0200

    add array project

---
+++ practices-cs/arrays/Solution.cs
@@ -0,0 +1,29 @@
+namespace Arrays;
+
+public class Solution
+{
+    public int[] Sort(int[] input)
+    {
+        // Enter your solution here
+    }
+
+    public int Max(int[] input)
+    {
+        // Enter your solution here
+    }
+
+    public int Min(int[] input)
+    {
+        // Enter your solution here
+    }
+
+    public int Sum(int[] input)
+    {
+        // Enter your solution here
+    }
+
+    public double Average(int[] input)
+    {
+        // Enter your solution here
+    }
+}
