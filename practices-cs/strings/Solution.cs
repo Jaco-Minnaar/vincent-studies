59126cf989e3eace65a27cc6c0267d6095b6808c
Vincent Roberts <robertsvincent57@gmail.com>
2024-09-30 11:05:41 +0200

    do solution

--- practices-cs/strings/Solution.cs
+++ practices-cs/strings/Solution.cs
@@ -4,15 +4,30 @@ public static class Solution
 {
     public static bool IsPalindrome(string word)
     {
-        // Vincent's solution here
+        var w = word.ToLower();
 
-        return false;
+        // Vincent's solution here
+        if (ReverseString(w) != w)
+        {
+            return false;
+        }
+        else 
+        {
+            return true;
+        }
     }
 
     public static string ReverseString(string word)
     {
         // Vincent's solution here
+        var s = "";
 
-        return "";
+        for (int i = word.Length - 1; i >= 0; i--)
+        {
+            var letter = word [i];
+            var newS = s + letter;
+             s = newS;
+        }
+        return s;
     }
 }
