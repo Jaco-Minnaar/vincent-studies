b870386505f3cf42479542004af583aa23d564d4
Jaco Minnaar <jminnaar@bscglobal.com>
2024-09-30 08:46:06 +0200

    add strings project outline

---
+++ practices-cs/strings/Program.cs
@@ -0,0 +1,32 @@
+﻿using Strings;
+
+(string, bool)[] words =
+[
+    ("Anna", true),
+    ("Vincent", false),
+    ("kayak", true),
+    ("anal", false),
+    ("super", false),
+    ("racecar", true),
+    ("jam", false),
+    ("rotator", true),
+    ("mitochondria", false)
+];
+
+foreach (var (word, isPalindrome) in words)
+{
+    var actual = Solution.IsPalindrome(word);
+    if (actual != isPalindrome)
+    {
+        Console.WriteLine(
+            "Incorrect. Word: {0}. Expected: {1}. Actual: {2}",
+            word,
+            isPalindrome,
+            actual
+        );
+        return 1;
+    }
+}
+
+Console.WriteLine("All words are correct. Congratulations!");
+return 0;
---
+++ practices-cs/strings/Solution.cs
@@ -0,0 +1,11 @@
+namespace Strings;
+
+public static class Solution
+{
+    public static bool IsPalindrome(string word)
+    {
+        // Vincent's solution here
+
+        return false;
+    }
+}
